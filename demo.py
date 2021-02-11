af7a8e235021c392adaa24623957146e0c6a39b3
Саша К <pokemongo.cops@gmail.com>
2021-02-11 02:52:25 +0000

    Added api key

---
+++ demo.py
@@ -0,0 +1,6 @@
+from facepplib import FacePP
+
+api_key = 'o7rhF6OZEJDMYwWrAPjhT-fZ2g3iu1bP'
+api_secret = 'MazgWDEXoDC05CyJhvxHjXOratdvhi-H'
+
+facepp = FacePP(api_key=api_key, api_secret=api_secret)
