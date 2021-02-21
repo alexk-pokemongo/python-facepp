d219b63b0c743f19ea5ef58a31c0e760f7c73cfd
Саша К <pokemongo.cops@gmail.com>
2021-02-21 02:19:00 +0000

    revise

--- recognition.py
+++ recognition.py
@@ -17,12 +17,14 @@ def build_annotation_table():
   plt.figure(figsize=(max_samples, num_persons))
 
   images=[]
+  image_files=[]
   colors=[(0,0,255),(0,255,0),(255,0,0),(0,255,255),(255,0,255),(255,255,0)]
 
   colorsize = (64,64)
 
   for p in range(num_persons):
     images.append([])
+    image_files.append([])
     
   files = glob2.glob(os.path.join(dataset_dir, "*.jpg")) 
 
@@ -37,6 +39,7 @@ def build_annotation_table():
     if(person_id.isdigit()):
       person_id = int(person_id) 
       images[person_id].append(img)
+      image_files[person_id].append(f)
       print("Appending person= %d sample = %d" %(person_id,len(images[person_id])-1))
 
   idx=0
@@ -61,10 +64,31 @@ def build_annotation_table():
     
   #save
   plt.savefig('yuka_table.png')
-  return images,colors
+  return image_files,colors
 
+def argmax(iterable):
+    return max(enumerate(iterable), key=lambda x: x[1])
 
-def run_recognition(images,colors):
+def compare_vs(app,img_file,reference_vec):
+  scores = []
+  num_persons = len(reference_vec)
+  for i in range(num_persons):
+    f = reference_vec[i] 
+    cmp_ = app.compare.get(image_file1=f,image_file2=img_file)
+    confidence = cmp_.confidence
+    thresholds = cmp_.thresholds
+    print("f=%s confidence=%d" % (f,confidence))
+    print('thresholds', '=', json.dumps(thresholds, indent=4))
+    scores.append(confidence)
+    pass
+
+  ret = (idx,val) = argmax(scores)
+  print("Result for: %s Index= %d confidence = %f",(img_file,idx,val))
+
+  return ret
+
+#toDo - in memory
+def run_recognition(image_files,colors):
   #and finally recognition
   api_key = 'o7rhF6OZEJDMYwWrAPjhT-fZ2g3iu1bP'
   api_secret = 'MazgWDEXoDC05CyJhvxHjXOratdvhi-H'
@@ -75,7 +99,7 @@ def run_recognition(images,colors):
 
   reference_vec = []
   for i in range(num_persons):
-    reference_vec.append(images[i][reference[i]])
+    reference_vec.append(image_files[i][reference[i]])
 
   app = FacePP(api_key=api_key, api_secret=api_secret)
   img_file = '/dataset/yuka_group.jpg'
@@ -100,6 +124,13 @@ def run_recognition(images,colors):
     h =face_rect['height']
     #cut out a face
     imcrop = image.crop((x0,y0,x0+w,y0+h))
-
+    #long term: convert to base64 ?
+    #fine if we just use file system for now
+    compare_path = "test_%d.jpg" % idx
+    imcrop.save(compare_path)
     #compare vs. imgs
+    compare_result = compare_vs(app,compare_path,reference_vec)
+    
+
+    #highlight face
     pass
